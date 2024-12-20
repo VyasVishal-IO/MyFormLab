9bcd8ecee140f54fa10719317b67c70b781ff010
VyasVishal-IO <vyasvishal.dev@gmail.com>
2024-12-20 14:52:26 +0530

    Update page.tsx

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -62,38 +62,38 @@ function StatsCards(props: StatsCardProps) {
     <div className="w-full pt-8 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
       <StatsCard
         title="Total visits"
-        icon={<LuView className="text-blue-600" />}
+        icon={<LuView className="text-white-600" />}
         helperText="All time form visits"
         value={data?.visits.toLocaleString() || ""}
         loading={loading}
-        className="shadow-md shadow-blue-600"
+        className=""
       />
 
       <StatsCard
         title="Total submissions"
-        icon={<FaWpforms className="text-yellow-600" />}
+        icon={<FaWpforms className="text-white-600" />}
         helperText="All time form submissions"
         value={data?.submissions.toLocaleString() || ""}
         loading={loading}
-        className="shadow-md shadow-yellow-600"
+        className=""
       />
 
       <StatsCard
         title="Submission rate"
-        icon={<HiCursorClick className="text-green-600" />}
+        icon={<HiCursorClick className="text-white-600" />}
         helperText="Visits that result in form submission"
         value={data?.submissionRate.toLocaleString() + "%" || ""}
         loading={loading}
-        className="shadow-md shadow-green-600"
+        className=""
       />
 
       <StatsCard
         title="Bounce rate"
-        icon={<TbArrowBounce className="text-red-600" />}
+        icon={<TbArrowBounce className="text-white-600" />}
         helperText="Visits that leaves without interacting"
         value={data?.submissionRate.toLocaleString() + "%" || ""}
         loading={loading}
-        className="shadow-md shadow-red-600"
+        className=""
       />
     </div>
   );
